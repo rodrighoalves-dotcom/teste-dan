985f22f7382bae38fc255ace4f6b2f33c058d35a
rodrighoalves-dotcom <rodrighoalves@gmail.com>
2026-08-01 12:19:51 -0300

    Delete GoAwake_SLA_Monitor_3.2.14_invalidado_injetado directory

--- GoAwake_SLA_Monitor_3.2.14_invalidado_injetado/shared/logger.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var SLA_LOG = Object.freeze({
-  info: function(message, data) { console.info('[SLA]', message, data === undefined ? '' : data); },
-  warn: function(message, data) { console.warn('[SLA]', message, data === undefined ? '' : data); },
-  error: function(message, error) { console.error('[SLA]', message, error === undefined ? '' : error); }
-});
